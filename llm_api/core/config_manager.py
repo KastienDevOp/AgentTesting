5e21b77450be1e0ca15058afb7e18266759d5f32
KastienDevOp215 <69985629+KastienDevOp@users.noreply.github.com>
2025-05-23 23:35:59 -0400

    Delete llm_api directory

--- llm_api/core/config_manager.py
+++
@@ -1,6 +0,0 @@
-class ConfigManager:
-    def __init__(self):
-        pass
-
-    def load_config(self):
-        pass
--- llm_api/interfaces/base_provider.py
+++
@@ -1,6 +0,0 @@
-class BaseProvider:
-    def __init__(self):
-        pass
-
-    def chat_completion(self, messages):
-        pass
